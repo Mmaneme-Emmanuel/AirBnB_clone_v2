7a03963b341dff148f9f6e6310494858869bb79f
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-03-27 02:25:51 +0100

    AirBnB_clone_v2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""This is the state class"""
-
-import os
-from sqlalchemy import create_engine
+""" new class for sqlAlchemy """
+from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import BaseModel, Base
-from models.city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
 from models.state import State
+from models.city import City
 from models.user import User
 from models.place import Place
 from models.review import Review
@@ -14,19 +14,21 @@ from models.amenity import Amenity
 
 
 class DBStorage:
+    """ create tables in environmental"""
     __engine = None
     __session = None
 
     def __init__(self):
-        user = os.getenv("HBNB_MYSQL_USER")
-        passwd = os.getenv("HBNB_MYSQL_PWD")
-        db = os.getenv("HBNB_MYSQL_DB")
-        host = os.getenv("HBNB_MYSQL_HOST")
-        env = os.getenv("HBNB_ENV")
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
 
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
                                       .format(user, passwd, host, db),
                                       pool_pre_ping=True)
+
         if env == "test":
             Base.metadata.drop_all(self.__engine)
 
@@ -37,7 +39,7 @@ class DBStorage:
         """
         dic = {}
         if cls:
-            if isinstance(cls, str):
+            if type(cls) is str:
                 cls = eval(cls)
             query = self.__session.query(cls)
             for elem in query:
@@ -50,7 +52,7 @@ class DBStorage:
                 for elem in query:
                     key = "{}.{}".format(type(elem).__name__, elem.id)
                     dic[key] = elem
-        return dic
+        return (dic)
 
     def new(self, obj):
         """add a new element in the table
@@ -66,7 +68,7 @@ class DBStorage:
         """delete an element in the table
         """
         if obj:
-            self.__session.delete(obj)
+            self.session.delete(obj)
 
     def reload(self):
         """configuration
