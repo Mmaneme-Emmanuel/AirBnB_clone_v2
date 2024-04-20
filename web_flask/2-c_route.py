4b9b075ac151c47e7227ad8dd78ffd080058d58a
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-20 01:25:35 +0100

    task 0 - 4

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application."""
+""" Starts a Flash Web Application C is FUN"""
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/',strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb',strict_slashes=False)
-def display_hbnb():
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     """ Prints a Message when /hbnb is called """
     return 'HBNB'
 
 
-@app.route('/c/<text>',strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    return "C"+text.replace('_','')
+    """ Prints a Message when /c is called """
+    return "C " + text.replace('_', ' ')
 
-
-"""Run the code if main"""
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
