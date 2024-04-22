e6719cb5fb94e0f47dde139225f4aa744637631a
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-22 11:39:10 +0100

    state.py

--- models/state.py
+++ models/state.py
@@ -14,21 +14,14 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
-
-
-@property
-def cities(self):
-    var = models.storage.all()
-    lista = []
-    result = []
-    for key in var:
-        city = key.replace('.', ' ')
-        city = shlex.split(city)
-        if city[0] == 'City':
-            lista.append(var[key])
-            for elem in lista:
-                if elem.state_id == self.id:
-                    result.append(elem)
-                    return result
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("city", backref="state", cascade="all, deletes")
+    else:
+        @property
+        def cities(self):
+            # Check if the storage engine is not DBStorage
+            city_list = []
+            for city in models.storage.all().values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
