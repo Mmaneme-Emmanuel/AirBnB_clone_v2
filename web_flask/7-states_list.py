b42b0fb52786e3ab5a1d0fb953a7d49d46f1ee88
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-22 21:07:01 +0100

    0x04. AirBnB clone

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
-from flask import Flask
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
-@app
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """close the storage on tear down"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
