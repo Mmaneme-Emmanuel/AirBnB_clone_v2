5f471b44d83280447b6f4d6fb8f015c0e2402ac7
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-23 19:11:49 +0100

    task 6

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """This is the file storage class for AirBnB"""
 import json
+from os import getenv
+from models.base_model import Base
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.city import City
 from models.place import Place
 from models.review import Review
-import shlex
-from os import getenv
+from models.state import State
+from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 
 
 class FileStorage:
--- models/state.py
+++ models/state.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
 import models
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from models.city import City
-import shlex
 from os import getenv
 
 
